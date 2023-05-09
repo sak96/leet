b716adfcc0ff52441d7b6d6e37f83cb20b377b6a
sak96 <26397224+sak96@users.noreply.github.com>
2023-05-09 13:39:38 +0530

    Add mat diagonal sum

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod all_one;
 pub mod dota;
 pub mod longest_obstacle;
 pub mod vowel;
+pub mod mat_diagonal_sum;
---
+++ src/mat_diagonal_sum.rs
@@ -0,0 +1,48 @@
+#![allow(dead_code)]
+
+impl Solution {
+    pub fn diagonal_sum(mat: Vec<Vec<i32>>) -> i32 {
+        let mut sum = 0;
+        let len = mat.len();
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..len {
+            let j = len - i - 1;
+            sum += mat[i][i];
+            if i != j {
+                sum += mat[i][j];
+            }
+        }
+        sum
+    }
+}
+
+pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let mat = [[1, 2, 3].into(), [4, 5, 6].into(), [7, 8, 9].into()].into();
+        assert_eq!(Solution::diagonal_sum(mat), 25);
+    }
+
+    #[test]
+    fn case2() {
+        let mat = [
+            [1, 1, 1, 1].into(),
+            [1, 1, 1, 1].into(),
+            [1, 1, 1, 1].into(),
+            [1, 1, 1, 1].into(),
+        ]
+        .into();
+        assert_eq!(Solution::diagonal_sum(mat), 8);
+    }
+
+    #[test]
+    fn case3() {
+        let mat = [[5].into()].into();
+        assert_eq!(Solution::diagonal_sum(mat), 5);
+    }
+}
