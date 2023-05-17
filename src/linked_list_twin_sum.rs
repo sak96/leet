8a7f491f6f81b2970140612687ab38f0bda113eb
sak96 <26397224+sak96@users.noreply.github.com>
2023-05-17 14:13:26 +0530

    Reverse first list instead of second one.

--- src/linked_list_twin_sum.rs
+++ src/linked_list_twin_sum.rs
@@ -1,34 +1,28 @@
 impl Solution {
     pub fn pair_sum(mut head: Option<Box<ListNode>>) -> i32 {
-        let mut half_ptr = head.as_mut().unwrap() as *mut Box<ListNode>;
-        let mut full_ptr = head.as_mut().unwrap().next.as_mut().unwrap() as *mut Box<ListNode>;
-        #[allow(unused_assignments)]
-        let mut second_half = None;
+        let mut node = head.take().expect("minimum 2 nodes expected");
+        head = node.next.take();
+        let mut reversed_half = Some(node);
+        let mut ptr = head.as_mut().expect("minimum 2 nodes expected") as *mut Box<ListNode>;
 
-        // travel to half
-        unsafe {
-            while (*full_ptr).next.is_some() {
-                half_ptr = (*half_ptr).next.as_mut().unwrap() as *mut Box<ListNode>;
-                full_ptr =
-                    (*full_ptr).next.as_mut().unwrap().next.as_mut().unwrap() as *mut Box<ListNode>;
+        // reverse first half
+        // first half reached when ptr.next is none
+        while unsafe { (*ptr).next.is_some() } {
+            unsafe {
+                ptr = (*ptr).next.as_mut().unwrap().next.as_mut().unwrap() as *mut Box<ListNode>;
             }
-            second_half = (*half_ptr).next.take();
-        }
-
-        // reverse second half;
-        let mut reversed_second_half = None;
-        while let Some(mut node) = second_half.take() {
-            second_half = node.next.take();
-            node.next = reversed_second_half;
-            reversed_second_half = Some(node);
+            let mut node = head.take().unwrap();
+            head = node.next.take();
+            node.next = reversed_half;
+            reversed_half = Some(node);
         }
 
         // get max sum of pairs.
         let mut max = 0;
-        while let (Some(mut start), Some(mut end)) = (head, reversed_second_half) {
+        while let (Some(mut start), Some(mut end)) = (head, reversed_half) {
             max = std::cmp::max(max, start.val + end.val);
             head = start.next.take();
-            reversed_second_half = end.next.take();
+            reversed_half = end.next.take();
         }
 
         max
