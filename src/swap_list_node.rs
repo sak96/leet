ebe29fe00c716ad80ec5fd5cb0d38bdb30d68ae1
sak96 <26397224+sak96@users.noreply.github.com>
2023-05-17 12:42:31 +0530

    Use unsafe to solve swapping 2 nodes.

--- src/swap_list_node.rs
+++ src/swap_list_node.rs
@@ -1,59 +1,25 @@
 impl Solution {
     pub fn swap_nodes(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        // find length
-        let mut len = 0;
-        let mut cur_ptr = head.as_ref();
-        while let Some(node) = cur_ptr {
-            len += 1;
-            cur_ptr = node.next.as_ref();
-        }
-
-        // no swap if longer
-        // k <= n hence below not applicable
-        // if len <= k {
-        //     return head;
-        // }
-
-        // get index of swapping
-        use std::cmp::Ordering;
-        let i = k - 1;
-        let j = len - k;
-        let (mut low, mut high) = match i.cmp(&j) {
-            Ordering::Less => (i, j),
-            Ordering::Greater => (j, i),
-            _ => return head,
-        };
-
-        // get first node
-        let mut left_part = &mut head;
-        while low > 0 {
-            left_part = &mut left_part.as_mut().unwrap().next;
-            low -= 1;
-            high -= 1;
-        }
-
-        let mut right_part = left_part.as_mut().unwrap().next.take();
-        high -= 1;
-
-        // get node two
-        {
-            let mut right_part = &mut right_part;
-            while high > 0 {
-                right_part = &mut right_part.as_mut().unwrap().next;
-                high -= 1;
+        let mut first_k_node = head.as_mut().unwrap() as *mut Box<ListNode>;
+        // get first kth node
+        for _ in 1..k {
+            unsafe {
+                first_k_node = (*first_k_node).next.as_mut().unwrap();
             }
-
-            // swap the values
-            std::mem::swap(
-                &mut left_part.as_mut().unwrap().val,
-                &mut right_part.as_mut().unwrap().val,
-            );
         }
 
-        // assign right_part back
-        left_part.as_mut().unwrap().next = right_part;
+        // get second kth node
+        let mut end_node = first_k_node;
+        let mut second_k_node = head.as_mut().unwrap() as *mut Box<ListNode>;
+        unsafe {
+            while (*end_node).next.is_some() {
+                end_node = (*end_node).next.as_mut().unwrap();
+                second_k_node = (*second_k_node).next.as_mut().unwrap();
+            }
+            // swap the node values
+            std::mem::swap(&mut (*first_k_node).val, &mut (*second_k_node).val);
+        }
 
-        // return
         head
     }
 }
