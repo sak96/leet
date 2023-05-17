6b1b056ce53f081725e7534acf07531a3ac7dce3
sak96 <26397224+sak96@users.noreply.github.com>
2023-05-17 12:09:41 +0530

    Use pointer and stuff for linked list twin sum.

--- src/linked_list_twin_sum.rs
+++ src/linked_list_twin_sum.rs
@@ -1,16 +1,34 @@
 impl Solution {
     pub fn pair_sum(mut head: Option<Box<ListNode>>) -> i32 {
-        let mut storage = std::collections::VecDeque::with_capacity(100_000);
-        while let Some(node) = head.take() {
-            storage.push_front(node.val);
-            head = node.next;
+        let mut half_ptr = head.as_mut().unwrap() as *mut Box<ListNode>;
+        let mut full_ptr = head.as_mut().unwrap().next.as_mut().unwrap() as *mut Box<ListNode>;
+        #[allow(unused_assignments)]
+        let mut second_half = None;
+
+        // travel to half
+        unsafe {
+            while (*full_ptr).next.is_some() {
+                half_ptr = (*half_ptr).next.as_mut().unwrap() as *mut Box<ListNode>;
+                full_ptr =
+                    (*full_ptr).next.as_mut().unwrap().next.as_mut().unwrap() as *mut Box<ListNode>;
+            }
+            second_half = (*half_ptr).next.take();
         }
 
+        // reverse second half;
+        let mut reversed_second_half = None;
+        while let Some(mut node) = second_half.take() {
+            second_half = node.next.take();
+            node.next = reversed_second_half;
+            reversed_second_half = Some(node);
+        }
+
+        // get max sum of pairs.
         let mut max = 0;
-        while !storage.is_empty() {
-            let a = storage.pop_front().unwrap();
-            let b = storage.pop_back().unwrap();
-            max = std::cmp::max(max, a + b);
+        while let (Some(mut start), Some(mut end)) = (head, reversed_second_half) {
+            max = std::cmp::max(max, start.val + end.val);
+            head = start.next.take();
+            reversed_second_half = end.next.take();
         }
 
         max
