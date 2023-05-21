e47b12fbbd8b84b89ec97fb4523dd37d0b2790cc
sak96 <26397224+sak96@users.noreply.github.com>
2023-05-21 12:01:48 +0530

    Update nodes when finding.

--- src/graph_bipartite.rs
+++ src/graph_bipartite.rs
@@ -1,14 +1,12 @@
 impl Solution {
     #[inline]
-    pub fn find_group_id(groups: &[usize], mut value: usize) -> usize {
-        let mut parent;
-        loop {
-            parent = groups[value];
-            if value == parent {
-                break parent;
-            }
-            value = parent;
+    pub fn find_group_id(groups: &mut [usize], value: usize) -> usize {
+        let mut parent = groups[value];
+        if value != parent {
+            parent = Self::find_group_id(groups, parent);
+            groups[value] = parent;
         }
+        parent
     }
 
     pub fn is_bipartite(graph: Vec<Vec<i32>>) -> bool {
@@ -17,10 +15,10 @@ impl Solution {
         for (cur, dest_nodes) in graph.into_iter().enumerate() {
             if let Some(dest_first) = dest_nodes.first() {
                 // join all destination to same group.
-                let parent_first = Self::find_group_id(&groups, *dest_first as usize);
-                let parent_cur = Self::find_group_id(&groups, cur);
+                let parent_first = Self::find_group_id(&mut groups, *dest_first as usize);
+                let parent_cur = Self::find_group_id(&mut groups, cur);
                 for dest in dest_nodes {
-                    let dest_parent = Self::find_group_id(&groups, dest as usize);
+                    let dest_parent = Self::find_group_id(&mut groups, dest as usize);
                     if dest_parent == parent_cur {
                         // cur node and destination are in same group
                         return false;
