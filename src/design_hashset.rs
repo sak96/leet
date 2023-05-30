c7b8470acbdd3fa0f3cec8da631a74b8aac73174
sak96 <26397224+sak96@users.noreply.github.com>
2023-05-30 20:29:49 +0530

    Don't use hash map

--- src/design_hashset.rs
+++ src/design_hashset.rs
@@ -1,21 +1,26 @@
-#[derive(Default)]
-pub struct MyHashSet (std::collections::HashSet<i32>);
+pub struct MyHashSet(Box<[bool]>);
 
 impl MyHashSet {
+    const MAX_LENGTH: usize = 1_000_001;
+
+    #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
-        Self::default()
+        Self(vec![false; Self::MAX_LENGTH].into_boxed_slice())
     }
 
     pub fn add(&mut self, key: i32) {
-        self.0.insert(key);
+        debug_assert!((key as usize) < Self::MAX_LENGTH);
+        self.0[key as usize] = true;
     }
 
     pub fn remove(&mut self, key: i32) {
-        self.0.remove(&key);
+        debug_assert!((key as usize) < Self::MAX_LENGTH);
+        self.0[key as usize] = false;
     }
 
     pub fn contains(&self, key: i32) -> bool {
-        self.0.contains(&key)
+        debug_assert!((key as usize) < Self::MAX_LENGTH);
+        self.0[key as usize]
     }
 }
 
@@ -26,13 +31,17 @@ mod tests {
     #[test]
     fn leet1() {
         let mut my_hashset = MyHashSet::new();
-        my_hashset.add(1); 
-        my_hashset.add(2); 
-        assert!(my_hashset.contains(1)); 
-        assert!(!my_hashset.contains(3), "not found"); 
-        my_hashset.add(2); 
-        assert!(my_hashset.contains(2)); 
-        my_hashset.remove(2); 
-        assert!(!my_hashset.contains(2), "already removed"); 
+        my_hashset.add(1);
+        my_hashset.add(2);
+        assert!(my_hashset.contains(1));
+        assert!(!my_hashset.contains(3), "not found");
+        my_hashset.add(2);
+        assert!(my_hashset.contains(2));
+        my_hashset.remove(2);
+        assert!(!my_hashset.contains(2), "already removed");
+        my_hashset.add(1_000_000);
+        assert!(my_hashset.contains(1_000_000));
+        my_hashset.remove(1_000_000);
+        assert!(!my_hashset.contains(1_000_000), "already removed");
     }
 }
