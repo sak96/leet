2a1ee6b87cf5f6bc190483e0fd6d73f53ad65a3e
sak96 <26397224+sak96@users.noreply.github.com>
2023-06-16 16:12:17 +0530

    Use Vec for faster access

--- src/number_of_ways_to_reorder_array_to_get_same_bst.rs
+++ src/number_of_ways_to_reorder_array_to_get_same_bst.rs
@@ -1,36 +1,31 @@
 //! Solution for https://leetcode.com/problems/number-of-ways-to-reorder-array-to-get-same-bst
-
-use std::collections::HashMap;
 impl Solution {
     const MOD: i64 = 10i64.pow(9) + 7;
 
-    pub fn combination(n: i64, r: i64, combinations: &mut HashMap<(i64, i64), i64>) -> i64 {
-        let key = (n, r);
-        if let Some(&value) = combinations.get(&key) {
+    pub fn combination(n: usize, r: usize, combinations: &mut [Vec<Option<i64>>]) -> i64 {
+        if let Some(value) = combinations[n][r] {
             value
         } else {
             let value = if r == 0 || n == r {
                 1
-            } else if r == 1 {
-                n
             } else {
                 Self::combination(n - 1, r - 1, combinations)
                     + Self::combination(n - 1, r, combinations)
             } % Self::MOD;
-            combinations.insert(key, value);
+            combinations[n][r] = Some(value);
             value
         }
     }
 
-    pub fn nums_of_ways_(nums: &mut [i32], combinations: &mut HashMap<(i64, i64), i64>) -> i64 {
+    pub fn nums_of_ways_(nums: &mut [i32], combinations: &mut [Vec<Option<i64>>]) -> i64 {
         if nums.is_empty() {
             1
         } else {
             let (x, nums) = nums.split_at_mut(1);
-            let n = nums.len() as i64;
+            let n = nums.len();
             nums.sort_by_key(|i| x[0] < *i);
             let (left, right) = nums.split_at_mut(nums.partition_point(|i| x[0] > *i));
-            let r = left.len().min(right.len()) as i64;
+            let r = left.len().min(right.len());
             let mut result = Self::combination(n, r, combinations);
             result *= Self::nums_of_ways_(left, combinations);
             result %= Self::MOD;
@@ -39,7 +34,8 @@ impl Solution {
         }
     }
     pub fn num_of_ways(mut nums: Vec<i32>) -> i32 {
-        Self::nums_of_ways_(&mut nums, &mut HashMap::new()) as i32 - 1
+        let len = nums.len();
+        Self::nums_of_ways_(&mut nums, &mut vec![vec![None; len]; len]) as i32 - 1
     }
 }
 pub struct Solution;
