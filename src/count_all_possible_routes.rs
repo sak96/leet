bff7397ac849b3b9e04730ffb72002dcd395d531
sak96 <26397224+sak96@users.noreply.github.com>
2023-06-26 10:31:53 +0530

    Solve count_all_possible_routes

---
+++ src/count_all_possible_routes.rs
@@ -0,0 +1,50 @@
+//! Solution for https://leetcode.com/problems/count-all-possible-routes
+//! 1575. Count All Possible Routes
+
+impl Solution {
+    const MOD: i32 = 1e9 as i32 + 7;
+    pub fn count_routes(locations: Vec<i32>, start: i32, finish: i32, fuel: i32) -> i32 {
+        let len = locations.len();
+        let mut dp = vec![vec![0; fuel as usize + 1]; len];
+        dp[finish as usize].iter_mut().for_each(|x| *x = 1);
+        for fuel_left in 0..dp[0].len() {
+            for i in 0..len {
+                for j in 0..len {
+                    if i == j {
+                        continue;
+                    }
+                    let fuel_consumed = (locations[i] - locations[j]).abs() as usize;
+                    if fuel_consumed <= fuel_left {
+                        dp[i][fuel_left] += dp[j][fuel_left - fuel_consumed];
+                        dp[i][fuel_left] %= Self::MOD;
+                    }
+                }
+            }
+        }
+        dp[start as usize][fuel as usize]
+    }
+}
+
+pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(vec![2,3,6,8,4], 1, 3, 5, 4)]
+    #[case(vec![4,3,1], 1, 0, 6, 5)]
+    #[case(vec![5,2,1], 0, 2, 3, 0)]
+    fn case(
+        #[case] locations: Vec<i32>,
+        #[case] start: i32,
+        #[case] finish: i32,
+        #[case] fuel: i32,
+        #[case] expected: i32,
+    ) {
+        let actual = Solution::count_routes(locations, start, finish, fuel);
+        assert_eq!(actual, expected);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub mod can_make_arithmetic_progression_from_sequence;
 pub mod check_is_straight_line;
 pub mod combination_sum;
 pub mod combination_sum_ii;
+pub mod count_all_possible_routes;
 pub mod count_and_say;
 pub mod count_negative_numbers_in_a_sorted_matrix;
 pub mod create_max_number;
