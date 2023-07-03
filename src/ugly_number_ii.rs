3a84371ce41c4dfa5eaa13b0d3e707d94e7be9e0
sak96 <26397224+sak96@users.noreply.github.com>
2023-07-03 18:14:09 +0530

    Use loop instead

--- src/ugly_number_ii.rs
+++ src/ugly_number_ii.rs
@@ -3,24 +3,20 @@ impl Solution {
         let mut ugly_num = Vec::with_capacity(n as usize);
         ugly_num.push(1);
         // ith index to be qualified as next multiple
-        let mut multiples = (0, 0, 0);
+        // reason: ugly[i] * multiple < ugly[i+1] * multiple
+        let mut multiples = [0, 0, 0];
         for _ in 1..n {
-            // reason: ugly[i] * multiple < ugly[i+1] * multiple
             let nums = [
-                ugly_num[multiples.0] * 2,
-                ugly_num[multiples.1] * 3,
-                ugly_num[multiples.2] * 5,
+                ugly_num[multiples[0]] * 2,
+                ugly_num[multiples[1]] * 3,
+                ugly_num[multiples[2]] * 5,
             ];
             let min = nums.iter().min().copied().unwrap();
             ugly_num.push(min);
-            if min == nums[0] {
-                multiples.0 += 1;
-            }
-            if min == nums[1] {
-                multiples.1 += 1;
-            }
-            if min == nums[2] {
-                multiples.2 += 1;
+            for (i, val) in nums.iter().enumerate() {
+                if min.eq(val) {
+                    multiples[i] += 1;
+                }
             }
         }
         ugly_num.pop().unwrap()
