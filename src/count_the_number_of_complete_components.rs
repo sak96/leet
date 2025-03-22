42ef610daf60484e9abceb07d3e9ed51becbfac3
sak96 <26397224+sak96@users.noreply.github.com>
2025-03-22 11:48:24 +0530

    Reduce by one loop

--- src/count_the_number_of_complete_components.rs
+++ src/count_the_number_of_complete_components.rs
@@ -15,8 +15,7 @@ impl Solution {
     pub fn count_complete_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
         let n = n as usize;
         let mut set: Vec<_> = (0..n).collect();
-        let mut edge_count: Vec<_> = vec![1; n];
-        let mut node_count: Vec<_> = vec![0; n];
+        let mut edge_count: Vec<_> = vec![1; n]; // assume it has loop to itself
         for edge in edges {
             let (m, n) = (edge[0] as usize, edge[1] as usize);
             let m_par = Self::get_parent(&mut set, m);
@@ -25,17 +24,18 @@ impl Solution {
             edge_count[m] += 1;
             edge_count[n] += 1;
         }
-        for i in 0..n {
-            let p = Self::get_parent(&mut set, i);
-            node_count[p] += 1;
-        }
+        let mut edge_sum: Vec<_> = vec![0; n]; // add all edge to parent
+        let mut node_count: Vec<_> = vec![0; n]; // node count per parent
         for (i, edges) in edge_count.into_iter().enumerate() {
             let p = Self::get_parent(&mut set, i);
-            if node_count[p] != edges {
-                node_count[p] = 0;
-            }
+            node_count[p] += 1;
+            edge_sum[p] += edges;
         }
-        node_count.iter().filter(|a| **a > 0).count() as i32
+        node_count
+            .into_iter()
+            .zip(edge_sum)
+            .filter(|(n, e)| n > &0 && n * n == *e)
+            .count() as i32
     }
 }
 
