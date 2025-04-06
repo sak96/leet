b660854e40e85db61f330222151e890f71934ba6
sak96 <26397224+sak96@users.noreply.github.com>
2025-04-06 12:52:26 +0530

    Bit preprocessing

--- src/valid_palindrome.rs
+++ src/valid_palindrome.rs
@@ -3,31 +3,22 @@
 
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let mut bytes = std::collections::VecDeque::from(s.as_bytes().to_vec());
-        while !bytes.is_empty() {
-            let ch1 = loop {
-                match bytes.pop_front() {
-                    Some(ch) if ch.is_ascii_alphanumeric() => {
-                        break ch.to_ascii_lowercase();
-                    }
-                    None => return true,
-                    _ => {}
-                }
+        let mut bytes: std::collections::VecDeque<u8> = s
+            .bytes()
+            .filter_map(|c| c.is_ascii_alphanumeric().then_some(c.to_ascii_lowercase()))
+            .collect();
+        loop {
+            let Some(ch1) = bytes.pop_front() else {
+                break true;
             };
-            let ch2 = loop {
-                match bytes.pop_back() {
-                    Some(ch) if ch.is_ascii_alphanumeric() => {
-                        break ch.to_ascii_lowercase();
-                    }
-                    None => return true,
-                    _ => {}
-                }
+            let Some(ch2) = bytes.pop_back() else {
+                break true;
             };
+            dbg!(ch1 as char, ch2 as char);
             if ch1 != ch2 {
-                return false;
+                break false;
             }
         }
-        true
     }
 }
 
